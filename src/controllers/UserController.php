8da2566f9a19e38e9eded304bf730f1e945a648e
Giovanni Policene <giovannicpoli@gmail.com>
2025-05-25 20:50:29 -0300

    feat: UserController

---
+++ src/controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once __DIR__ . '/../services/UserService.php';
+require_once __DIR__ . '/../exceptions/ConflictException.php';
+
+class UserController {
+
+    public static function createUser() {
+
+        $request = json_decode(file_get_contents('php://input'), true);
+
+        try {
+
+            $response = UserService::createUser($request);
+            http_response_code(201);
+            echo json_encode([
+                'msg' => 'ok',
+                'user' => $response
+            ]);
+
+        } catch (InvalidArgumentException $e) {
+            http_response_code(400);
+            echo json_encode(['error' => $e->getMessage()]);
+
+        } catch (ConflictException $e) {
+            http_response_code(409);
+            echo json_encode(['error' => $e->getMessage()]);
+
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);
+        }
+        
+    } 
+
+    public static function getById(){
+
+    }
+
+}
+
+?>
