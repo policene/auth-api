2cb06abf6e67dd2e18ceaccbc0a281251008cfd4
Giovanni Policene <giovannicpoli@gmail.com>
2025-05-25 21:01:46 -0300

    feat: UserRepository

--- src/repositories/UserRepository.php
+++ src/repositories/UserRepository.php
@@ -1,2 +1,31 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../models/User.php';
+
+class UserRepository {
+
+    public static function saveUser (User $user) {
+        $pdo = getConnection();
+        $stmt = $pdo->prepare('INSERT INTO users (name, last_name, email, password) VALUES (:name, :lastName, :email, :password)');
+        $stmt->execute([
+            'name' => $user->getName(),
+            'lastName' => $user->getLastName(),
+            'email' => $user->getEmail(),
+            'password' => $user->getPassword()
+        ]);
+    }
+
+    public static function findByEmail (string $email) : bool {
+        $pdo = getConnection();
+        $stmt = $pdo->prepare('SELECT 1 FROM users WHERE email = :email LIMIT 1');
+        $stmt->execute(['email' => $email]);
+        return (bool) $stmt->fetchColumn();
+    }
+
+}
+
+?>
+
 
 
